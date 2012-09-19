92da9b1e8323cc3199ed0840f32e741e72afc3f9
wenke <liangwenke.com@gmail.com>
2012-09-19 16:03:08 +0800

    fix typo

--- lib/generators/social_share_button/templates/config/initializers/social_share_button.rb
+++ lib/generators/social_share_button/templates/config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook weibo douban qq tqq t163 tsohu baidu_tieba baidu_cang)
+  config.allow_sites = %w(twitter facebook weibo douban qq tqq t163 tsohu tieba baidu)
 end
